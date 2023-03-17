067e7b0282b04097702fd14434d652e9ad994bda
JOUINI Boubaker <boubaker.jouini@esprit.tn>
2023-03-17 13:30:54 +0100

    added prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  useTabs: false,
+  printWidth: 120,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'all',
+  jsxBracketSameLine: false,
+  bracketSpacing: true,
+  semi: false,
+  arrowParens: 'avoid',
+}
